4f5f3cc237f83c0ec437a6c969f78a9e511ac4dc
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-09-24 10:58:04 +0300

    initial project setup

---
+++ EfCorePlayground.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCorePlayground.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
